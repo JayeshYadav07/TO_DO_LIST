2366694a9323345007e7c5a832a9059eecdd13bb
JayeshYadav57 <jayeshyadav8080@gmail.com>
2023-05-22 12:43:02 +0530

    Moveup and Movedown functionality added.

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./List.css";
-export default function List({ data, deleteTask, editTask }) {
+export default function List({ data, deleteTask, editTask, moveUp, moveDown }) {
     function handleDelete(id) {
         deleteTask(id);
     }
     function handleEdit(item) {
         editTask(item);
+    }
+    function handleUp(item) {
+        moveUp(item.id);
+    }
+    function handleDown(item) {
+        moveDown(item.id);
     }
     return (
         <div>
@@ -14,9 +20,11 @@ export default function List({ data, deleteTask, editTask }) {
                     <div className="row" key={item.id}>
                         <p>{item.task}</p>
                         <button onClick={() => handleDelete(item.id)}>
-                            ❌
+                            ❌Delete
                         </button>
-                        <button onClick={() => handleEdit(item)}>✏️</button>
+                        <button onClick={() => handleEdit(item)}>✏️Edit</button>
+                        <button onClick={() => handleUp(item)}>⬆️Up</button>
+                        <button onClick={() => handleDown(item)}>⬇️Down</button>
                     </div>
                 );
             })}
--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -7,7 +7,7 @@ export default function ToDo() {
     const [data, setData] = useState([]);
     const [newTask, setNewTask] = useState(null);
     function addTask(task) {
-        setData([...data, { id: data.length, task }]);
+        setData([...data, { id: data.length + 1, task }]);
     }
     function deleteTask(id) {
         setData(data.filter((task) => task.id !== id));
@@ -21,11 +21,39 @@ export default function ToDo() {
         );
         setNewTask(null);
     }
+    function moveUp(id) {
+        let newArray = [...data];
+        for (let i = 1; i < newArray.length; i++) {
+            if (newArray[i].id === id) {
+                let temp = { ...newArray[i], id: newArray[i - 1].id };
+                newArray[i] = { ...newArray[i - 1], id: newArray[i].id };
+                newArray[i - 1] = temp;
+                setData(newArray);
+            }
+        }
+    }
+    function moveDown(id) {
+        let newArray = [...data];
+        for (let i = 0; i < newArray.length - 1; i++) {
+            if (newArray[i].id === id) {
+                let temp = { ...newArray[i], id: newArray[i + 1].id };
+                newArray[i] = { ...newArray[i + 1], id: newArray[i].id };
+                newArray[i + 1] = temp;
+                setData(newArray);
+            }
+        }
+    }
     return (
         <div>
             <h3>To-Do list</h3>
             <Form addTask={addTask} newTask={newTask} updateTask={updateTask} />
-            <List data={data} deleteTask={deleteTask} editTask={editTask} />
+            <List
+                data={data}
+                deleteTask={deleteTask}
+                editTask={editTask}
+                moveUp={moveUp}
+                moveDown={moveDown}
+            />
         </div>
     );
 }
